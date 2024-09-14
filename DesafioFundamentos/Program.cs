99e2b288426284ff9c3b7da3adb8083521f93d4a
Louis Miller <114757766+rlouismiller@users.noreply.github.com>
2024-09-14 16:39:24 -0300

    Update Program.cs
    Sem mudanças, mas preferi atualizar com o código da máquina para evitar problemas

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -1,4 +1,4 @@
-﻿using DesafioFundamentos.Models;
+using DesafioFundamentos.Models;
 
 // Coloca o encoding para UTF8 para exibir acentuação
 Console.OutputEncoding = System.Text.Encoding.UTF8;
@@ -10,8 +10,10 @@ Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
                   "Digite o preço inicial:");
 precoInicial = Convert.ToDecimal(Console.ReadLine());
 
+
 Console.WriteLine("Agora digite o preço por hora:");
 precoPorHora = Convert.ToDecimal(Console.ReadLine());
+
 
 // Instancia a classe Estacionamento, já com os valores obtidos anteriormente
 Estacionamento es = new Estacionamento(precoInicial, precoPorHora);
